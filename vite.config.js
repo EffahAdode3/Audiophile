5c7f3ecea79cff272a8fac52a0e58ce4077521e3
EffahAdode3 <whatyousow00@gmail.com>
2025-06-22 20:24:54 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'vue-router'],
+          utils: ['zod']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'zod']
+  }
 }) 
